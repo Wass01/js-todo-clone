415e757eec3979ab97dc8d3058b89676aaf606c7
wass01 <maatallawassim01@gmail.com>
2021-03-17 16:11:27 +0100

    consegna fatta ma da migliorare

--- main.js
+++ main.js
@@ -3,12 +3,27 @@
 
 $(document).ready(function() {
   $("#btnAdd").click(function () {
+
+    // aggiungo gli elementi alla lista con input e button
     var lista = $("#list li").clone();
     var inputList = $("#inputList").val();
 
     lista.prepend(inputList);
     $("#todoList").append(lista);
 
+    // rimuovo gli elementi della lista
+    $(lista).click(function () {
+      $(this).remove();
+    });
 
+    // aggiungo elementi anche al tasto invio
+    lista.keyup(function(e){
+      var lista = $("#list li").clone();
+      var inputList = $("#inputList").val();
+      if (e.which == 13) {
+        lista.prepend(inputList);
+        $("#todoList").append(lista);
+      }
+    });
   });
 });
